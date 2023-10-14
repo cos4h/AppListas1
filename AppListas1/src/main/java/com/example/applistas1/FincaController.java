72bdb5d1571eb76e811bfa3241f3e4c77bab2e4d
r=cos(4θ) <olvadis2004@gmail.com>
2023-10-14 16:34:49 -0500

    commit 3
    generacion de las funciones registrar

--- AppListas1/src/main/java/com/example/applistas1/FincaController.java
+++ AppListas1/src/main/java/com/example/applistas1/FincaController.java
@@ -2,7 +2,10 @@ package com.example.applistas1;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+
+import java.util.LinkedList;
 
 public class FincaController {
   @FXML private TextField addressTextField;
@@ -10,6 +13,9 @@ public class FincaController {
   @FXML private TextField nameTextField;
   @FXML private TextField phoneTextField;
   @FXML private TextField propietaryTextField;
+  LinkedList<Cls_fincas> fincas = new LinkedList<>();
+  private int posicion;
+  private boolean sw;
 
   public void initialize() {
     codTextField.setPromptText("123");
@@ -31,9 +37,30 @@ public class FincaController {
     clean();
   }
   @FXML void saveOnButtonPressed(ActionEvent event) {
-
+    guardar(nameTextField.getText(), codTextField.getText(),
+            addressTextField.getText(), phoneTextField.getText(),
+            propietaryTextField.getText());
   }
   @FXML void searchOnButtonPressed(ActionEvent event) {
 
   }
-}
+
+  private void guardar(String cod, String name, String address,
+                      String phone, String propietary) {
+    if(cod.isEmpty() || name.isEmpty() || address.isEmpty() ||
+       phone.isEmpty() || propietary.isEmpty()) {
+      showMessage(Alert.AlertType.ERROR, "Debes ingresar todos los campos");
+      return ;
+    } else {
+      fincas.add(new Cls_fincas(name, cod, address, phone, propietary));
+      showMessage(Alert.AlertType.INFORMATION, "Finca registrada con exito");
+    }
+  }
+  private void showMessage(Alert.AlertType type, String messageToDisplay) {
+    Alert alert = new Alert(type);
+    alert.setContentText(messageToDisplay);
+    alert.show();
+  }
+  }
+
+
