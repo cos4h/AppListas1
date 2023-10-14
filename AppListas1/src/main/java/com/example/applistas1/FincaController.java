5b46c3d37ab0918c8decdb204d89650f5890d927
r=cos(4θ) <olvadis2004@gmail.com>
2023-10-14 15:34:25 -0500

    formulario
    creacion del formulario funcion limpiar

--- AppListas1/src/main/java/com/example/applistas1/FincaController.java
+++ AppListas1/src/main/java/com/example/applistas1/FincaController.java
@@ -1,5 +1,39 @@
 package com.example.applistas1;
 
-public class FincaController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+public class FincaController {
+  @FXML private TextField addressTextField;
+  @FXML private TextField codTextField;
+  @FXML private TextField nameTextField;
+  @FXML private TextField phoneTextField;
+  @FXML private TextField propietaryTextField;
+
+  public void initialize() {
+    codTextField.setPromptText("123");
+    nameTextField.setPromptText("La triguizar");
+    addressTextField.setPromptText("CL 98 Diagonal");
+    phoneTextField.setPromptText("3233234443");
+    propietaryTextField.setPromptText("Juanito");
+  }
+
+  private void clean() {
+    codTextField.clear();
+    nameTextField.clear();
+    addressTextField.clear();
+    propietaryTextField.clear();
+    phoneTextField.clear();
+  }
+
+  @FXML void cancelOnButtonPressed(ActionEvent event) {
+    clean();
+  }
+  @FXML void saveOnButtonPressed(ActionEvent event) {
+
+  }
+  @FXML void searchOnButtonPressed(ActionEvent event) {
+
+  }
 }
